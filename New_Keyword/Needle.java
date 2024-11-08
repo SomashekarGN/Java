fef95432cf9087c37dbff7801957128e2f4349e2
SomashekarGN <93174768+SomashekarGN@users.noreply.github.com>
2024-11-08 19:28:43 +0530

    Delete New_Keyword directory

--- New_Keyword/Needle.java
+++
@@ -1,9 +0,0 @@
-//3.Needle : material,length
-
-
-class Needle
-{
-
-	String material;
-	int length;
-}
--- New_Keyword/WaterBottle.java
+++
@@ -1,8 +0,0 @@
-//2.WaterBottle: size,shape
-
-class WaterBottle{
-
-int size;
-String shape;
-
-}
